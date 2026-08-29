57f98c912e4d8c7592d49f5b959e41c2fbda6e10
apm23 <80390905+apm23@users.noreply.github.com>
2026-08-30 00:11:01 +0900

    Make Velocity Hopper transfer every tick

---
+++ src/main/java/com/anjas/forceanvil/mixin/VelocityHopperSpeedMixin.java
@@ -0,0 +1,24 @@
+package com.anjas.forceanvil.mixin;
+
+import com.anjas.forceanvil.ForceAnvilMod;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.HopperBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(HopperBlockEntity.class)
+public abstract class VelocityHopperSpeedMixin {
+    @Shadow private int cooldownTime;
+
+    @Inject(method = "serverTick", at = @At("HEAD"))
+    private static void forceanvil$velocityHopperOneTick(Level level, BlockPos pos, BlockState state, HopperBlockEntity hopper, CallbackInfo ci) {
+        if (state.is(ForceAnvilMod.VELOCITY_HOPPER) && ((VelocityHopperSpeedMixin)(Object)hopper).cooldownTime > 1) {
+            ((VelocityHopperSpeedMixin)(Object)hopper).cooldownTime = 1;
+        }
+    }
+}
